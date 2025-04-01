63fc191aacc25f8e297150cb82d7cb24bcf1e3cd
nhpd <dmitry.klpv@gmail.com>
2025-04-01 16:56:26 +0400

    update packages to match deICS'ed network

--- proto-build/src/main.rs
+++ proto-build/src/main.rs
@@ -16,30 +16,30 @@ const WASMD_REPO: &str = "https://github.com/neutron-org/wasmd.git";
 const COMETBFT_REPO: &str = "https://github.com/cometbft/cometbft.git";
 const IBC_GO_REPO: &str = "https://github.com/cosmos/ibc-go.git";
 const ICS23_REPO: &str = "https://github.com/cosmos/ics23.git";
-const FEEMARKET_REPO: &str = "https://github.com/skip-mev/feemarket.git";
+const FEEMARKET_REPO: &str = "https://github.com/neutron-org/feemarket.git";
 const SLINKY_REPO: &str = "https://github.com/skip-mev/slinky.git";
 const INTERCHAIN_SECURITY_REPO: &str = "https://github.com/cosmos/interchain-security.git";
 const ADMIN_MODULE_REPO: &str = "https://github.com/neutron-org/admin-module.git";
 
 /// The Cosmos SDK commit or tag to be cloned and used to build the proto files
-const COSMOS_SDK_REV: &str = "v0.50.11-neutron";
+const COSMOS_SDK_REV: &str = "v0.50.13-neutron";
 
 /// The Neutron commit or tag to be cloned and used to build the proto files
-const NEUTRON_REV: &str = "fd655299cadfd9f94a7f5772e8c688886cec37fd";
+const NEUTRON_REV: &str = "46a5fb3fe2cf13b9526e6f676e49f98b20b73fd3";
 
 /// The wasmd commit or tag to be cloned and used to build the proto files
-const WASMD_REV: &str = "v0.53.2-neutron";
+const WASMD_REV: &str = "8ab684b5eff649e2e335a6e1b439ae8b72afd392";
 
 /// The cometbft commit or tag to be cloned and used to build the proto files
 const COMETBFT_REV: &str = "v0.38.17";
 
 /// The ibc-go commit or tag to be cloned and used to build the proto files
-const IBC_GO_REV: &str = "v8.5.2";
+const IBC_GO_REV: &str = "v8.7.0";
 
 /// The ics23 commit or tag to be cloned and used to build the proto files
 const ICS23_REV: &str = "go/v0.11.0";
 
-const FEEMARKET_REV: &str = "v1.1.1";
+const FEEMARKET_REV: &str = "4ffcf3d434644a5499fff5a81d16ebea62e9eac1";
 
 const SLINKY_REV: &str = "v1.2.0";
 
